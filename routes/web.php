5a11a609d84b61e582d7ec3ecc35749729ee5fa4
SandraDeviaArge <sandradeviarge@gmail.com>
2024-02-22 14:59:35 +0700

    Perubahan Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return 'Hello Word';
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama Saya '.$name;
 });
